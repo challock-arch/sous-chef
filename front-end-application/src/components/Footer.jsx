e35f9abf84bd549051506e4e1b9a5a6f95a47566
Caleb <hallockdesigns.21@gmail.com>
2025-10-16 19:56:18 -0500

    Updated structure and added styling and positioning for footer

--- front-end-application/src/components/Footer.jsx
+++ front-end-application/src/components/Footer.jsx
@@ -1,8 +1,13 @@
 const Footer = () => {
     return (
-        <footer>
-            <a>GitHub</a>
-            <a>Portfolio</a>
+        <footer class="w-screen absolute bottom-0 p-3 bg-lime-950 text-white flex ">
+            <div class="align-left flex-auto basis-1/2">
+            <p>&copy; 2025 Caleb Hallock</p>
+            </div>
+            <div class="flex items-stretch basis-1/2">
+            <a class="basis-1/2 text-center">GitHub</a>
+            <a class="basis-1/2 text-center">Portfolio</a>      
+            </div>
         </footer>
     )
 }
