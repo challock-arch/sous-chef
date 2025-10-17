78513bcbbb47c547217c7e31a1592d441d465866
Caleb <hallockdesigns.21@gmail.com>
2025-10-17 10:24:32 -0500

    Added basic stylings to Home

--- front-end-application/src/pages/Home.jsx
+++ front-end-application/src/pages/Home.jsx
@@ -1,7 +1,22 @@
+import image from '../assets/images/pexels-goumbik-616484.jpg';
+
 const Home = () => {
     return (
         <>
-        This is my Home Page.
+        <div class="h-150 flex-col justify-center content-center p-3" style={{backgroundImage: `url(${image})`}}>
+        <div class="bg-stone-100/65 rounded-md p-5">
+            <h1 class="pb-5 text-4xl md:text-6xl text-center md:text-right md:pr-10 ">Welcome to Sous Chef!</h1>
+            <p class="text-lg md:text-xl text-center md:text-right md:pr-10">Your personal recipe manager and meal planner.</p>
+        </div>
+        </div>
+        <div class="p-3 pb-10">
+            <h2 class="text-4xl py-2">Recipe Walkthroughs</h2>
+                <p class="text-lg pb-4">Sous Chef provides users with a walkthrough experience of all their favorite recipes</p>
+            <h2 class="text-4xl pb-2">Meal Planning</h2>
+                <p class="text-lg pb-4">For those who are busy or struggle to find what they want to make each day, Sous Chef allows you to plan out meals for a week so you can be sure you know what you're making every night.</p>
+            <h2 class="text-4xl pb-2">Grocery Lists</h2>
+                <p class="text-lg pb-4">To ensure you have everything you'll need for your meals during the week, Sous Chef helps to create shopping lists that include everything right from your recipes so it's easy to keep track of everything!</p>
+        </div>
         </>
     )
 }
