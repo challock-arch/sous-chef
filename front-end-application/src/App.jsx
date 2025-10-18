33280eb306990b8d51473e6632c394d941efdbb8
Caleb <hallockdesigns.21@gmail.com>
2025-10-18 16:34:01 -0500

    Added MVP pages for recipes, meal plan, and grocery shopping list

--- front-end-application/src/App.jsx
+++ front-end-application/src/App.jsx
@@ -5,6 +5,9 @@ import Navigation from './components/Navigation'
 import Home from './pages/Home'
 import About from './pages/About'
 import Login from './pages/Login'
+import Recipes from './pages/Recipes'
+import MealPlanning from './pages/MealPlanning'
+import GroceryList from './pages/GroceryList'
 
 function App() {
 
@@ -16,6 +19,9 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/recipes" element ={<Recipes />} />
+      <Route path="/meal-plan" element ={<MealPlanning />} />
+      <Route path="/grocery-list" element ={<GroceryList />} />
     </Routes>
     <Footer/>
     </BrowserRouter>
