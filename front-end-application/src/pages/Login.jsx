2b0058a59989ccc9f5aadfcb18f617f06a36fcd1
Caleb <hallockdesigns.21@gmail.com>
2025-10-18 16:37:52 -0500

    refactored form page

--- front-end-application/src/pages/Login.jsx
+++ front-end-application/src/pages/Login.jsx
@@ -1,7 +1,21 @@
+import Button from "../components/Button";
+import FormLine from "../components/FormLine";
+
 const Login = () => {
     return (
         <>
-        This is my login page
+        <div class="grid p-10 place-content-center h-screen">
+        <div>
+            <h1 class="text-center text-4xl md:text-6xl">Login to get Started</h1>
+            <br />
+            <form>
+                <FormLine label="Username" type="text" name="username" value="" />
+                <FormLine label="Password" type="password" name="password" value="" />
+
+                <Button type="submit" msg="Submit" />
+            </form>
+        </div>
+        </div>
         </>
     )
 }
