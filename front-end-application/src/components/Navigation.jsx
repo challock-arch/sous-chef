06791d7d46b4624bac52621505333d983a21e781
Caleb <hallockdesigns.21@gmail.com>
2025-10-17 23:29:15 -0500

    Updated responsive layouts and added hamburger menu icon

--- front-end-application/src/components/Navigation.jsx
+++ front-end-application/src/components/Navigation.jsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
 import { Link } from "react-router";
+import Hamburger from 'hamburger-react';
 
 const Navigation = () => {
+
+    const [loggedIn, isLoggedIn] = useState(false);
+
+    const handleClick = () => {
+        isLoggedIn(!loggedIn);
+    }
+
     return (
         <header>
-            <nav class="flex w-full p-2 bg-lime-950 text-white rounded-md m-1 fixed">
-                <div class="flex-none">
+            <nav class="grid grid-cols-2 bg-lime-950 text-white rounded-b-md">
+                <div class="px-4 self-center">
                     <Link to="/">Logo Placeholder</Link>
                 </div>
-                <div class="flex flex-auto justify-end">
-                    <Link to="/" class="px-2">Home</Link>
-                    <Link to="about" class="px-2">About</Link>
-                    <Link to="login" class="px-2">Login</Link>
+                <div class="justify-self-end self-center md:hidden">
+                    <Hamburger size={20} color="white" />
+                </div>
+                <div class="hidden md:justify-self-end md:flex md:py-3 md:items-center">
+                    <Link to="/" class="px-4">Home</Link>
+                    <Link to="about" class="px-4">About</Link>
+                    <button class="px-4" onClick={handleClick}>{loggedIn ? "Logout" : "Login"}</button>
                 </div>
             </nav>
         </header>
