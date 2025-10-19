b235a17528444f8e3e6269b234b114276a581a9d
Caleb <hallockdesigns.21@gmail.com>
2025-10-19 10:56:39 -0500

    Removed TODO

--- front-end-application/src/components/RecipeCard.jsx
+++ front-end-application/src/components/RecipeCard.jsx
@@ -1,17 +1,16 @@
 import { Link } from "react-router";
 import Button from "./Button";
 
-const RecipeCard = ({name, prepTime, cookTime, servings, calories}) => {
+const RecipeCard = ({id, name, prepTime, cookTime, servings, calories}) => {
     return (
-        <div class="bg-stone-200 rounded-md p-4">
-            {/* TODO: Add destructured props to function and feed into card*/}
+        <div key={id} itemID={id} class="bg-stone-200 rounded-md p-4">
             <h1 class="font-semibold">{name}</h1>
             <h2 class="text-stone-400">Serves: {servings}</h2>
             <h2 class="text-stone-400">Calories: {calories}</h2>
             <h2 class="text-stone-400">Prep Time: {prepTime}</h2>
             <h2 class="text-stone-400">Cook Time: {cookTime}</h2>
             <br />
-            <Link to="/recipe/{id}"><Button type="button" msg="Make It!" /></Link>
+            <Link to={`/recipe/${id}`}><Button type="button" msg="Make It!" /></Link>
         </div>
     )
 }
